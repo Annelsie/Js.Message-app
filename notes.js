f41090b0b16a0100a59a2af6c2287ab6ae6c0404
Annelsie <atimelsie@gmail.com>
2019-08-06 12:12:58 +0300

    i prefered white chocolate

---
+++ notes.js
@@ -0,0 +1,26 @@
+//git clone
+
+//dir
+//git init
+//git status
+//git add index.html
+//git add . >>>>add all files
+// git reset >>>> remove all files 
+//git rm --cached index.html >>> to remove a specific file
+//git add.
+//git commit -m "1st commit(small comment)" >>>
+//git log
+//git shows m on file to show changes made on the code be4 committing
+//git commit -m "added .gitignore"
+//git remote add origin<<name>> //https://........<<repository url>>
+//git push
+//git push --set-upstream origin master
+
+//<<creating branches>> 
+//git branch develop
+//git branch --list
+//git branch --list --all develop
+
+//<<move from one branch to another>>
+//git checkout develop
+
